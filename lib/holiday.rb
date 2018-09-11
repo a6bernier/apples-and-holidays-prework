523ab1b236ab019b3126a238a7f77a26fcc20a9d
Andrew Bernier <a6bernier@gmail.com>
2018-09-11 23:27:10 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,36 +25,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  def all_winter_holiday_supplies(holiday_hash)
-    supply_array = []
-    holiday_hash[:winter].each do |holiday, supplies|
-      supply_array << supplies
-    end
-  supply_array.flatten
-  end
-
-  def all_supplies_in_holidays(holiday_hash)
-
-    holiday_hash.each do |key, value|
-      season = key.to_s
-      puts season.capitalize + ":"
-
-      value.each do |key2, value2|
-        holiday = key2.to_s
-        holiday_split = holiday.split("_")
-        holiday_list =[]
-
-        holiday_split.each do |word|
-          holiday_list << word.capitalize
-        end
-
-        supplies = value2.join(", ")
-
-        puts "  " + holiday_list.join(" ") + ": " + supplies
-      end
-    end
-  end
-end
+  
 
 
 def all_holidays_with_bbq(holiday_hash)
